20ee8941f0ca0f237e29d1e505ce21a979845f31
Jack Bond-Preston <jack_b_p@btinternet.com>
2012-09-29 20:31:58 +0100

    Removed irrelevant code until Gui is implemented

--- GuiHandler.java
+++ GuiHandler.java
@@ -10,24 +10,10 @@ import cpw.mods.fml.common.network.IGuiHandler;
 public class GuiHandler implements IGuiHandler {
 
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		System.out.println("getServerGuiElement called.");
-		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		
-		if (tileEntity instanceof TileEntityShadowCatcher) {
-			return new ContainerShadowCatcher((TileEntityShadowCatcher)tileEntity, player.inventory);
-		}
-		
 		return null;
 	}
 
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		System.out.println("getClientGuiElement called.");
-		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		
-		if (tileEntity instanceof TileEntityShadowCatcher) {
-			return new GuiShadowCatcher(player.inventory, (TileEntityShadowCatcher)tileEntity, ((TileEntityShadowCatcher) tileEntity).shadows);
-		}
-		
 		return null;
 	}
 
