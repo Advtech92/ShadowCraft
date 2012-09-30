cc19ba8abe9c35d1384b9aa37f87789cbb4c2910
Jack Bond-Preston <jack_b_p@btinternet.com>
2012-09-30 11:52:22 +0100

    Removed unecessary code

--- block/ShadowCatcher.java
+++ block/ShadowCatcher.java
@@ -84,50 +84,6 @@ public class ShadowCatcher extends BlockContainer{
 		ShadowCraft.scLog.info("Shadows: " + te.shadows);
 		return true;
 	}
-	
-	
-	@Override
-	public void breakBlock(World world, int x, int y, int z, int i, int j) {
-		dropItems(world, x, y, z);
-		
-		super.breakBlock(world, x, y, z, i, j);
-	}
-	
-	public void dropItems(World world, int x, int y, int z) {
-		Random rand = new Random();
-
-		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-
-		if(!(tileEntity instanceof IInventory)){
-			return;
-		}
-
-		IInventory inventory = (IInventory) tileEntity;
-
-		for(int i = 0; i < inventory.getSizeInventory(); i++){
-			ItemStack item = inventory.getStackInSlot(i);
-
-			if(item != null && item.stackSize > 0){
-				float rx = rand.nextFloat() * 0.6F + 0.1F;
-				float ry = rand.nextFloat() * 0.6F + 0.1F;
-				float rz = rand.nextFloat() * 0.6F + 0.1F;
-
-				EntityItem entity_item = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
-
-				if(item.hasTagCompound()){
-					entity_item.item.setTagCompound((NBTTagCompound) item.getTagCompound().copy());
-				}
-
-				float factor = 0.5F;
-
-				entity_item.motionX = rand.nextGaussian() * factor;
-				entity_item.motionY = rand.nextGaussian() * factor + 0.2F;
-				entity_item.motionZ = rand.nextGaussian() * factor;
-				world.spawnEntityInWorld(entity_item);
-				item.stackSize = 0;
-			}
-		}
-	}
 
 	@Override
 	public TileEntity createNewTileEntity(World var1) {
